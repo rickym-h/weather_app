8e07a4895773856695906063acdc671738b12eec
rickym-h <richardhull11@gmail.com>
2022-07-27 16:07:13 +0100

    initial api call to get weather data added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("test")
+let weatherData;
+async function getWeatherData(locationQuery) {
+    if ((!locationQuery) ||(locationQuery.length === 0)) {
+        locationQuery = "London";
+    }
+    const API_KEY = "d6c2aeb1081496ab644469e9d5cc827c";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${locationQuery}&APPID=${API_KEY}`;
+    const response = await fetch(url, {cors: true});
+    weatherData = await response.json();
+}
+getWeatherData();
